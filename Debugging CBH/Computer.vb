dd25407d9b97106205098dfba082ba2361669610
Clive B Harrison <clive_b_harrison@outlook.com>
2021-11-12 23:07:58 +0000

    Add project files.

---
+++ Debugging CBH/Computer.vb
@@ -0,0 +1,48 @@
+﻿Public Class Computer
+    Implements IDisposable
+
+    Private disposedValue As Boolean
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+
+    Public ReadOnly Property FreeMemory() As String
+        Get
+            'Using the My namespace
+            Return Format((
+            My.Computer.Info.AvailablePhysicalMemory.ToString _
+            / 1024),
+            "#,###,##0") & " K"
+        End Get
+    End Property
+    Public ReadOnly Property TotalMemory() As String
+        Get
+            'Using the My namespace
+            Return Format(
+            My.Computer.Info.TotalPhysicalMemory.ToString / 1024,
+            "#,###,##0") & " K"
+        End Get
+    End Property
+End Class
---
+++ Debugging/Computer.vb
@@ -0,0 +1,48 @@
+﻿Public Class Computer
+    Implements IDisposable
+
+    Private disposedValue As Boolean = False    ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                ' TODO: free other state (managed objects).
+            End If
+
+            ' TODO: free your own state (unmanaged objects).
+            ' TODO: set large fields to null.
+        End If
+        Me.disposedValue = True
+    End Sub
+
+#Region " IDisposable Support "
+    ' This code added by Visual Basic to correctly implement
+    ' the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in
+        ' Dispose(ByVal disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+    Public ReadOnly Property FreeMemory() As String
+        Get
+            'Using the My namespace
+            Return Format(( _
+                My.Computer.Info.AvailablePhysicalMemory.ToString \ 1024), _
+                "#,###,##0") & " K"
+        End Get
+    End Property
+
+    Public ReadOnly Property TotalMemory() As String
+        Get
+            'Using the My namespace
+            Return Format(( _
+                My.Computer.Info.TotalPhysicalMemory.ToString \ 1024), _
+                "#,###,##0") & " K"
+        End Get
+    End Property
+
+End Class
